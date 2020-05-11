36c8fc68518b50185aefdf1fff21b1aad850c706
Martin Pfannemüller <git@pfannemueller.de>
2020-05-11 13:27:30 +0200

    Add #!/bin/bash to shell script

--- Enhancing_a_Communication_System_with_Adaptive_Behavior_using_REACT/startSwim.sh
+++ Enhancing_a_Communication_System_with_Adaptive_Behavior_using_REACT/startSwim.sh
@@ -1,1 +1,3 @@
+#!/bin/bash
+
 cd /headless/seams-swim/swim/simulations/swim && ../../src/swim swim.ini -u Cmdenv -c sim -n ..:../../src/:../../../queueinglib:../../src -lqueueinglib -s --cmdenv-redirect-output=true -r 0
