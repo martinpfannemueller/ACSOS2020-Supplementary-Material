5d6f836af39c3c46101e380d878661a1548c6328
Martin Pfannemüller <martin.pfannemueller@uni-mannheim.de>
2020-05-12 12:14:41 +0000

    Update AOS, remove aggregationMode, use Clarknet trace

--- Enhancing_a_Communication_System_with_Adaptive_Behavior_using_REACT/startSwim.sh
+++ Enhancing_a_Communication_System_with_Adaptive_Behavior_using_REACT/startSwim.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cd /headless/seams-swim/swim/simulations/swim && ../../src/swim swim.ini -u Cmdenv -c sim -n ..:../../src/:../../../queueinglib:../../src -lqueueinglib -s --cmdenv-redirect-output=true -r 0
+cd /headless/seams-swim/swim/simulations/swim && ../../src/swim swim.ini -u Cmdenv -c sim -n ..:../../src/:../../../queueinglib:../../src -lqueueinglib -s --cmdenv-redirect-output=true -r '$trace="traces/clarknet-http-105m-l70.delta"'
